8407f2569f4ce9853e80f08da0279f2c13cb42d5
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-19 14:51:16 +0100

    Added Head component and custom font

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
+import Head from '../components/head.js'
+import styles from '../styles/home.module.css'
+
 export default function Home() {
   return (
     <div>
-      <h1>Your To Do List</h1>
-      <ul>
-        <li>Type your to do into the input field</li>
-        <li>Click the item to mark it as complete</li>
-        <li>Click the X to remove it from your list.</li>
-      </ul>
-      <form>
-        <input type="Text" placeholder="New Item" />
-        <button>Add</button>
-      </form>
+      <Head/>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Your To Do List</h1>
+        <ul className={styles.list}>
+          <li className={styles['list-item']}>Type your to do into the input field</li>
+          <li className={styles['list-item']}>Click the item to mark it as complete</li>
+          <li className={styles['list-item']}>Click the X to remove it from your list.</li>
+        </ul>
+        <form className={styles.form}>
+          <input type="Text" placeholder="New Item" className={styles.input} />
+          <button className={styles.button}>Add</button>
+        </form>
+      </div>
     </div>
   )
 }
