a274f9d480fd5dddffbb6c4a8c400f0250c7984c
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-19 14:37:06 +0100

    Added html

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <h1>Your To Do List</h1>
+      <ul>
+        <li>Type your to do into the input field</li>
+        <li>Click the item to mark it as complete</li>
+        <li>Click the X to remove it from your list.</li>
+      </ul>
+      <form>
+        <input type="Text" placeholder="New Item" />
+        <button>Add</button>
+      </form>
+    </div>
   )
 }
